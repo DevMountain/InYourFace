08fd1fb00ff62ba00abd589c13d03fe4258238de
Joshua Howland <development@jkhowland.com>
2014-06-05 01:04:10 -0600

    Initial Commit

---
+++ InYourFace/IFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IFAppDelegate.h
+//  InYourFace
+//
+//  Created by Joshua Howland on 6/5/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InYourFace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InYourFace
+//
+//  Created by Joshua Howland on 6/5/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IFAppDelegate class]));
+    }
+}
